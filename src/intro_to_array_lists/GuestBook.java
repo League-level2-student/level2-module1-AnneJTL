9fe9fba69954fbf03c88e42cb7bb2c7a92630ac5
AnneJTL <anne.chatin@jointheleague.org>
2018-10-15 18:51:17 -0700

    GuestBook and intro_to_array_lists

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,80 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GuestBook implements ActionListener {
+    private JFrame window;
+    private JPanel panel;
+    private JButton addButton, viewButton;
+    private List<String> allNames;
+
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+    private void GUI(){
+        window = new JFrame("ArrayList");
+        window.setSize(new Dimension(400,300));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+
+        Dimension buttonSize = new Dimension(150,100);
+        addButton = new JButton("Add Name");
+        addButton.addActionListener(this);
+        addButton.setPreferredSize(buttonSize);
+        viewButton = new JButton("View Names");
+        viewButton.addActionListener(this);
+        viewButton.setPreferredSize(buttonSize);
+
+
+        panel.add(addButton);
+        panel.add(viewButton);
+
+        window.add(panel);
+        window.setVisible(true);
+
+    }
+
+    public GuestBook(){
+        allNames = new ArrayList<>();
+        GUI();
+    }
+    public static void main(String[] args) {
+        new GuestBook();
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addButton) {
+            // When the add name button is clicked, display an input dialog that asks the user to enter a name.
+            // Add that name to an ArrayList.
+            String newName = JOptionPane.showInputDialog("Which name do you want to add ?");
+            allNames.add(newName);
+
+        } else if ((JButton) e.getSource() == viewButton) {
+            // When the "View Names" button is clicked, display a message dialog that displays
+            // all the names added to the list. Format the list as follows:
+            // Guest #1: Bob Banders
+            // Guest #2: Sandy Summers
+            // Guest #3: Greg Ganders
+            // Guest #4: Donny Doners
+            StringBuilder displayAllNames = new StringBuilder();
+            for (int i = 1; i <= allNames.size(); i++){
+                displayAllNames.append("Guest #" + i + ": " + allNames.get(i-1) + '\n');
+            }
+            JOptionPane.showMessageDialog(null, displayAllNames);
+
+        } else {
+            JOptionPane.showOptionDialog(null, "There is something wired", "Error",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE,null,null,null);
+        }
+
+    }
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,52 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		List<String> stringArray = new ArrayList<>();
+
 		//2. Add five Strings to your list
-		
+		stringArray.add("Monday");
+		stringArray.add("Tuesday");
+		stringArray.add("Wednesday");
+		stringArray.add("Friday");
+		stringArray.add("Thursday");
+
 		//3. Print all the Strings using a standard for-loop
+		System.out.println("Standard for-loop");
+		for (int i = 0; i < stringArray.size(); i++) {
+			System.out.println(stringArray.get(i));
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		System.out.println("Standard for-each loop");
+		for (String day : stringArray) {
+			System.out.println(day);
+		}
+
 		//5. Print only the even numbered elements in the list.
+		System.out.println("Print only the even numbered elements in the list.");
+		for (int i = 0; i < stringArray.size(); i++) {
+			if (i % 2 == 0) {
+				System.out.println(stringArray.get(i));
+			}
+		}
 		
 		//6. Print all the Strings in reverse order.
-		
+		System.out.println("Print all the Strings in reverse order.");
+		for (int i = stringArray.size(); i > 0; i--) {
+				System.out.println(stringArray.get(i-1));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("Print only the Strings that have the letter 'e' in them.");
+		for (String day : stringArray) {
+			if (day.contains("e")) {
+				System.out.println(day);
+			}
+		}
 	}
 }
