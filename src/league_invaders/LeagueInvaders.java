ac902eb322dd1241c2f87964dc43c8d3edec652e
AnneJTL <anne.chatin@jointheleague.org>
2018-10-16 15:16:53 -0700

    league_invaders - Part 1 done

--- src/league_invaders/LeagueInvaders.java
+++ src/league_invaders/LeagueInvaders.java
@@ -1,7 +1,25 @@
 package league_invaders;
 
-public class LeagueInvaders {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class LeagueInvaders {
+    private JFrame window;
+    private final int windowWidth = 500;
+    private final int windowHeight = 800;
+
+    public static void main(String[] args) {
+        new LeagueInvaders().setup();
+
+    }
+
+    public LeagueInvaders() {
+        window = new JFrame("League Invaders");
+        window.setSize(windowWidth, windowHeight);
+
+    }
+
+    private void setup(){
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 }
